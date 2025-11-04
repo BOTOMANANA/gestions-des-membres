970dd15c2db9d79858b444c47b2b43d83c981e7f
BOTOMANANA <antoniobotomanana@gmail.com>
2025-11-04 12:09:33 +0300

    first commit

--- lib/presentation/widgets/member_item_widget.dart
+++ lib/presentation/widgets/member_item_widget.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:association_appli/presentation/colors/Light_theme_colors.dart';
+import 'package:association_appli/presentation/fonts/app_fonts.dart';
 import 'package:association_appli/presentation/pages/members/members_profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:association_appli/domain/entities/member_entity.dart';
@@ -23,16 +27,23 @@ class MemberItemWidget extends StatelessWidget {
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(14),
-            border: Border.all(width: 1.5, color: Colors.blue.shade300),
+            border: Border.all(
+              width: 1.0,
+              color: LightThemeColors.colorPrimary.withOpacity(0.50),
+            ),
           ),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
             child: Row(
               children: [
                 CircleAvatar(
-                  radius: 22,
-                  backgroundColor: Colors.grey.shade200,
-                  backgroundImage: const AssetImage('assets/icons/avatar.png'),
+                  radius: 24,
+                  backgroundColor: Colors.white,
+                  child: Image.asset(
+                    'assets/images/profilegirl.png',
+                    width: 100,
+                    height: 100,
+                  ),
                 ),
                 const SizedBox(width: 12),
 
@@ -42,27 +53,30 @@ class MemberItemWidget extends StatelessWidget {
                     children: [
                       Text(
                         memberEntity.fullName,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 14,
-                          color: Colors.black87,
+                        style: AppFonts.robotoFont(
+                          size: 14.0,
+                          color: LightThemeColors.textBlack,
+                          weight: FontWeight.w600,
                         ),
                       ),
                       const SizedBox(height: 2),
                       Text(
                         memberEntity.country,
-                        style: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontSize: 12,
+                        style: AppFonts.robotoCondensedFont(
+                          size: 12.0,
+                          color: LightThemeColors.textSemiBlack,
                         ),
                       ),
                     ],
                   ),
                 ),
 
-                Text(
-                  "20/01/2026",
-                  style: TextStyle(color: Colors.grey.shade500, fontSize: 12),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 24.0),
+                  child: Text(
+                    "20/01/2026",
+                    style: TextStyle(color: Colors.grey.shade400, fontSize: 12),
+                  ),
                 ),
               ],
             ),
